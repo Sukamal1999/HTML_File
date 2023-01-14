d6f6c31f1ebce648b41242376f25c8cdafa18abb
Sukamal <sukamalmondal07@gmail.com>
2023-01-14 23:54:17 +0530

    https://github.com/Sukamal1999/HTML_File

--- dom.js
+++ dom.js
@@ -60,26 +60,23 @@
 
 //QWERY_SELECTOR
 
-var header = document.querySelector('#main-header');
-header.style.borderBottom = 'solid 4px #ccc';
+// 
 
-var input = Document.querySelector('input');
-input.value = 'Hello World'
+//QUERYSELECTOR_ALL
 
-var submit = document.querySelector('input[type="submit"]');
-submit.value="SEND"
+var titles = document.querySelectorAll(.'title');
 
-var item = document.querySelector('.list-group-item');
-item.style.color ='red';
+console.log(titles);
+titles[0].textContent = 'Hello' ;
 
-var lastItem =document.querySelector
-('.list-group=item:las-child');
-lastItem.style.color = 'blue';
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
 
-var secondItem =document.querySelector
-('.list-group=item:nth-child(2)');
-secondItem.style.color = 'green';
-
+for(var i=0 ; i<odd.length;i++)
+{
+    odd[i].style.backgroundColor = '#00ff00';
+    even[i].style.backgroundColor = '#ccc';
+}
 
 
 
